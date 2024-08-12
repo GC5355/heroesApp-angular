540970baf3fb6c914483d179720bb610bb94c0af
German <gercger5@gmail.com>
2024-08-12 15:11:19 -0300

    Comienzo seccion 15 CRUD Heroes

--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Hero } from '../interfaces/hero.interface';
-import { catchError, delay, Observable, of } from 'rxjs';
+import { catchError, delay, map, Observable, of } from 'rxjs';
 import { environments } from '../../../environments/environments';
 
 @Injectable({ providedIn: 'root' })
@@ -22,9 +22,26 @@ export class HeroesService {
       )
   }
 
-
   getSuggestions( query: string ): Observable<Hero[]> {
     return this.http.get<Hero[]>(`${ this.baseUrl }/heroes?q=${query}&_limit=6`);
+  }
+
+  addHero( hero : Hero ): Observable<Hero>{
+    return this.http.post<Hero>(`${this.baseUrl}/heroes`, hero)
+  }
+
+  updateHero( hero : Hero ): Observable<Hero>{
+    if( !hero.id ) throw Error ('Hero id is required')
+
+    return this.http.patch<Hero>(`${this.baseUrl}/heroes/${ hero.id }`, hero)
+  }
+
+  deleteHeroById( id : string ): Observable<boolean> {
+    return this.http.delete(`${this.baseUrl}/heroes/${ id }`)
+      .pipe(
+        catchError( err => of(false) ),
+        map( resp =>  true )
+      )
   }
 
 }
