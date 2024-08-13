ece2c981ed4c0aacb42768471ad6d8f86d2b3bf5
German <gercger5@gmail.com>
2024-08-13 14:59:10 -0300

    Fin secc. 16. Implemtentacion Guard

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -2,10 +2,10 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environments } from '../../../environments/environments';
 import { User } from '../interfaces/user.interface';
-import { Observable, tap } from 'rxjs';
+import { catchError, map, Observable, of, tap } from 'rxjs';
 
 @Injectable({providedIn: 'root'})
-export class ServiceNameService {
+export class AuthService {
 
   private baseUrl = environments.baseUrl;
   // Es privado para que nadie lo pueda modificar
@@ -20,12 +20,32 @@ export class ServiceNameService {
 
   login( email : string, password : string) : Observable<User> {
 
-
     return this.http.get<User>(`${ this.baseUrl }/users/1`)
       .pipe(
         tap( user => { this.user = user }),
-        tap( user => localStorage.setItem('token', user.id.toString()))
+        tap( user => localStorage.setItem('token', 'Hola soy pepito'))
       )
+  }
+
+  checkAuthentication(): Observable<boolean> {
+
+    if ( !localStorage.getItem('token') ) return of(false);
+
+    const token = localStorage.getItem('token');
+
+    return this.http.get<User>(`${ this.baseUrl }/users/1`)
+      .pipe(
+        tap( user => this.user = user ),
+        map( user => !!user ), // Verifica que tenga algo efectivamente
+        catchError( err => of(false) )
+      );
+
+  }
+
+
+  logout() {
+    this.user = undefined;
+    localStorage.clear();
   }
 
 
--- src/app/heroes/pages/layout-page/layout-page.component.ts
+++ src/app/heroes/pages/layout-page/layout-page.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../../auth/services/auth.service';
+import { User } from '../../../auth/interfaces/user.interface';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'hero-layout-page',
@@ -6,11 +9,28 @@ import { Component } from '@angular/core';
 })
 export class LayoutPageComponent {
 
+
   public sidebarItems = [
     { label: 'Listado', icons: 'label', url: './list' },
     { label: 'Anadir', icons: 'add', url: './new-hero' },
     { label: 'Buscar', icons: 'search', url: './search' }
   ]
 
+  /**
+   *
+   */
+  constructor(
+    private AuthService: AuthService,
+    private router: Router
+  ) {}
+
+  get user(): User | undefined {
+    return this.AuthService.currentUser;
+  }
+
+  onLogout(): void {
+    this.AuthService.logout()
+    this.router.navigate(['/auth/login'])
+  }
 
 }
